c11a070f48006431452ad94a1eb5e9d6e6f10d59
Sean Dennis <sdennis@geodigital.com>
2018-08-27 20:17:37 -0500

    Added a test case for testing the RandomEventFactory

--- SCSDataModuleTest.java
+++ SCSDataModuleTest.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Test class for exercising the data module.
  */
@@ -12,30 +14,36 @@ public class SCSDataModuleTest implements ISCSError{
 		SCSDataModuleTest whoCares = new SCSDataModuleTest();
 		CONSTR = args[0];
 		whoCares.WorkAround();
- 
 	}
 	public void WorkAround(){
 		SCSDataModule Thing = new SCSDataModule();
-				Thing.errorHandler = this;
-				Thing.setConnectionString(CONSTR);
-				Thing.connect();
-					//boolean BisConnected = Thing.isConnected();
-				if (Thing.isConnected() == true){
-					System.out.println("IT IS CONNECTED. YAY.");
-				}else{
-					System.out.println("IT IS NOT CONNECTED. DRAT.");
-					return;
-				}
-				Thing.setLanguage("ENG");
-				String DisplayText;
-				//DisplayText ="RANDOM";
-				DisplayText = Thing.getDisplayText("WELCOME");
-				System.out.println("The Display Text Is " + DisplayText);
+		Thing.errorHandler = this;
+		Thing.setConnectionString(CONSTR);
+		Thing.connect();
+		//boolean BisConnected = Thing.isConnected();
+		if (Thing.isConnected() == true){
+			System.out.println("IT IS CONNECTED. YAY.");
+		}else{
+			System.out.println("IT IS NOT CONNECTED. DRAT.");
+			return;
+		}
+		Thing.setLanguage("ENG");
+		String DisplayText;
+		//DisplayText ="RANDOM";
+		DisplayText = Thing.getDisplayText("WELCOME");
+		System.out.println("The Display Text Is " + DisplayText);
 
 		String[][] sLangs;
 		sLangs = Thing.getLanguages();
 		for(int i = 0; i < sLangs.length; i++){
 			System.out.println("Lang " + i + " " + sLangs[i][0] + " - " + sLangs[i][1]);
+		}//end for i
+		
+		RandomEventFactory ref = new RandomEventFactory(Thing);
+		ArrayList<RandomEvent> events = ref.getList();
+		
+		for(int i = 0; i < events.size(); i++){
+			System.out.println(events.get(i).toString());
 		}//end for i
 
 		Thing.disConnect();
