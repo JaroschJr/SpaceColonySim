f6b19acb3cd4c8d661ce7087316b7c86a97112ef
Joseph Jarosch <jaroschjr@gmail.com>
2019-02-14 22:50:57 -0600

    A very minor tweak, in witch unnecessary declarations are removed.

--- SpaceColonyGame.java
+++ SpaceColonyGame.java
@@ -6,13 +6,13 @@
  */
 public class SpaceColonyGame{
 	public boolean bIsOngoing;
-	Inventory iInv = null;
+	Inventory iInv;
 	
 	public int iTurnCount = 1;
 	
 	public int iMerchantCountDown = 0;
 	
-	public Population pop = null;
+	public Population pop;
 
 	public void gameReset(){
 		bIsOngoing = true;
