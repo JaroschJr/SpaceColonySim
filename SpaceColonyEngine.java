d86ea67607102e179c350afc4f8c32c666c0d694
Joseph Jarosch <jaroschjr@gmail.com>
2019-12-12 22:29:40 -0600

    The game pauses part-way through the start-of-turn report, between morale summary and inventory.

--- SpaceColonyEngine.java
+++ SpaceColonyEngine.java
@@ -395,6 +395,8 @@ public class SpaceColonyEngine implements ISCSError{
 		_ioman.lineOut(_scsdm.getDisplayText("TURN_REPORT_POPULATION") +getSpacer((20-_scsdm.getDisplayText("TURN_REPORT_POPULATION").length())- Integer.toString(SCG.pop.size()).length()) + SCG.pop.size());
 		_ioman.lineOut(_scsdm.getDisplayText("MORALE") +getSpacer((20-_scsdm.getDisplayText("MORALE").length())- Integer.toString(SCG.pop.size()).length()) + SCG.subMorale);
 		_ioman.lineOut(mManager.moraleReport(SCG,_scsdm));
+		_ioman.lineOut(_scsdm.getDisplayText("CONTINUE"));
+		 _ioman.stringIn("");
 		for(int i = 0; i<SCG.iInv.size(); i++){
 			String sOut = "";
 			Good gPrint = SCG.iInv.get(i);
